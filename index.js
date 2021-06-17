645aadd46be0f0ae713e0855bf3f0e50f938126f
Aditi Bhuteja <67166033+aditi-bhuteja@users.noreply.github.com>
2021-06-17 17:07:57 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function showMenu() {
+    document.getElementById("open_menu").style.display = "block";
+  }
+  function closeMenu() {
+    document.getElementById("open_menu").style.display = "none";
+  }
+
+// LOGIN/REGISTER PAGE
+var x=document.getElementById('login');
+		var y=document.getElementById('register');
+		var z=document.getElementById('btn');
+		function register()
+		{
+			x.style.left='-400px';
+			y.style.left='50px';
+			z.style.left='110px';
+		}
+		function login()
+		{
+			x.style.left='50px';
+			y.style.left='450px';
+			z.style.left='0px';
+		}
+var modal = document.getElementById('login-form');
+        window.onclick = function(event) 
+        {
+            if (event.target == modal) 
+            {
+                modal.style.display = "none";
+            }
+        }
